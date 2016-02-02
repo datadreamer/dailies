a6496d686d03e7432a2198a79c76a44e68ce0f5f
Aaron Siegel <datadreamerlabs@gmail.com>
2016-02-02 12:54:35 -0800

    added site header/nav to dailies section

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<title>datadreamer dailies</title>
+		<meta name="description" content="Daily creative coding sketches by Aaron Siegel.">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="../_css/normalize.min.css" />
+		<link rel="stylesheet" type="text/css" href="../_css/fonts.css" />
+		<link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic' />
+		<link rel="stylesheet" type="text/css" href="../_css/sitenew.css" />
+		<link rel="stylesheet" type="text/css" href="dailies.css" />
+
+		<script src="../_js/vendor/jquery-1.11.0.min.js"></script>
+    <script src="../_js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<script src="../_js/menu.js"></script>
+	</head>
+
+	<body style="overflow:scroll;">
+
+		<?php
+			include_once("../analyticstracking.php");
+			include("../_php/header.php");
+		?>
+
+		<div id="content">
+
+      <a href="20160202/">
+  			<div class="thumb" style="background-image: url('20160202/thumb.png')">
+  				<span class="date">2016 02 02<br/>Open/Close</span>
+  			</div>
+  		</a>
+  		<a href="20160201/">
+  			<div class="thumb" style="background-image: url('20160201/thumb.png')">
+  				<span class="date">2016 02 01<br/>Sequencer #2</span>
+  			</div>
+  		</a>
+  		<a href="20160131/">
+  			<div class="thumb" style="background-image: url('20160131/thumb.png')">
+  				<span class="date">2016 01 31<br/>Sequencer</span>
+  			</div>
+  		</a>
+  		<a href="20160130/">
+  			<div class="thumb" style="background-image: url('20160130/thumb.png')">
+  				<span class="date">2016 01 30<br/>Bloop #2</span>
+  			</div>
+  		</a>
+  		<a href="20160129/">
+  			<div class="thumb" style="background-image: url('20160129/thumb.png')">
+  				<span class="date">2016 01 29<br/>Bloop</span>
+  			</div>
+  		</a>
+  		<a href="20160128/">
+  			<div class="thumb" style="background-image: url('20160128/thumb.png')">
+  				<span class="date">2016 01 28<br/>Color Bars</span>
+  			</div>
+  		</a>
+  		<a href="20160127/">
+  			<div class="thumb" style="background-image: url('20160127/thumb.png')">
+  				<span class="date">2016 01 27<br/>Praystation #4</span>
+  			</div>
+  		</a>
+  		<a href="20160126/">
+  			<div class="thumb" style="background-image: url('20160126/thumb.png')">
+  				<span class="date">2016 01 26<br/>Praystation #3</span>
+  			</div>
+  		</a>
+  		<a href="20160125/">
+  			<div class="thumb" style="background-image: url('20160125/thumb.png')">
+  				<span class="date">2016 01 25<br/>Praystation #2</span>
+  			</div>
+  		</a>
+  		<a href="20160124/">
+  			<div class="thumb" style="background-image: url('20160124/thumb.png')">
+  				<span class="date">2016 01 24<br/>Praystation</span>
+  			</div>
+  		</a>
+  		<a href="20160123/">
+  			<div class="thumb" style="background-image: url('20160123/thumb.png')">
+  				<span class="date">2016 01 23<br/>North by Northwest</span>
+  			</div>
+  		</a>
+  		<a href="20160122/">
+  			<div class="thumb" style="background-image: url('20160122/thumb.png')">
+  				<span class="date">2016 01 22<br/>Turning Color</span>
+  			</div>
+  		</a>
+  		<a href="20160121/">
+  			<div class="thumb" style="background-image: url('20160121/thumb.png')">
+  				<span class="date">2016 01 21<br/>Turning</span>
+  			</div>
+  		</a>
+  		<a href="20160120/">
+  			<div class="thumb" style="background-image: url('20160120/thumb.png')">
+  				<span class="date">2016 01 20<br/>CMYK Zig Zags</span>
+  			</div>
+  		</a>
+  		<a href="20160119/">
+  			<div class="thumb" style="background-image: url('20160119/thumb.png')">
+  				<span class="date">2016 01 19<br/>Christopher Kane Zig Zags</span>
+  			</div>
+  		</a>
+  		<a href="20160118/">
+  			<div class="thumb" style="background-image: url('20160118/thumb.png')">
+  				<span class="date">2016 01 18<br/>Traceroute #2</span>
+  			</div>
+  		</a>
+  		<a href="20160117/">
+  			<div class="thumb" style="background-image: url('20160117/thumb.png')">
+  				<span class="date">2016 01 17<br/>Traceroute</span>
+  			</div>
+  		</a>
+  		<a href="20160116/">
+  			<div class="thumb" style="background-image: url('20160116/thumb.png')">
+  				<span class="date">2016 01 16<br/>Interstellar #4</span>
+  			</div>
+  		</a>
+  		<a href="20160115/">
+  			<div class="thumb" style="background-image: url('20160115/thumb.png')">
+  				<span class="date">2016 01 15<br/>Interstellar #3</span>
+  			</div>
+  		</a>
+  		<a href="20160114/">
+  			<div class="thumb" style="background-image: url('20160114/thumb.png')">
+  				<span class="date">2016 01 14<br/>Interstellar #2</span>
+  			</div>
+  		</a>
+  		<a href="20160113/">
+  			<div class="thumb" style="background-image: url('20160113/thumb.png')">
+  				<span class="date">2016 01 13<br/>Interstellar</span>
+  			</div>
+  		</a>
+  		<a href="20160112/">
+  			<div class="thumb" style="background-image: url('20160112/thumb.png')">
+  				<span class="date">2016 01 12<br/>Christopher Kane Rectangles #3</span>
+  			</div>
+  		</a>
+  		<a href="20160111/">
+  			<div class="thumb" style="background-image: url('20160111/thumb.png')">
+  				<span class="date">2016 01 11<br/>Christopher Kane Rectangles #2</span>
+  			</div>
+  		</a>
+  		<a href="20160110/">
+  			<div class="thumb" style="background-image: url('20160110/thumb.png')">
+  				<span class="date">2016 01 10<br/>Christopher Kane Rectangles</span>
+  			</div>
+  		</a>
+  		<a href="20160109/">
+  			<div class="thumb" style="background-image: url('20160109/thumb.png')">
+  				<span class="date">2016 01 09<br/>Breton Noise Simultaneous Contrast</span>
+  			</div>
+  		</a>
+  		<a href="20160108/">
+  			<div class="thumb" style="background-image: url('20160108/thumb.png')">
+  				<span class="date">2016 01 08<br/>Breton Noise</span>
+  			</div>
+  		</a>
+  		<a href="20160107/">
+  			<div class="thumb" style="background-image: url('20160107/thumb.png')">
+  				<span class="date">2016 01 07<br/>Exploding Dots</span>
+  			</div>
+  		</a>
+  		<a href="20160106/">
+  			<div class="thumb" style="background-image: url('20160106/thumb.png')">
+  				<span class="date">2016 01 06<br/>CMYK Wedges</span>
+  			</div>
+  		</a>
+  		<a href="20160105/">
+  			<div class="thumb" style="background-image: url('20160105/thumb.png')">
+  				<span class="date">2016 01 05<br/>CMYK Mesh Network</span>
+  			</div>
+  		</a>
+  		<a href="20160104/">
+  			<div class="thumb" style="background-image: url('20160104/thumb.png')">
+  				<span class="date">2016 01 04<br/>CMYK Additive Arcs</span>
+  			</div>
+  		</a>
+  		<a href="20160103/">
+        <div class="thumb" style="background-image: url('20160103/thumb.png')">
+          <span class="date">2016 01 03<br/>CMYK Additive Mesh</span>
+        </div>
+      </a>
+  		<a href="20160102/">
+        <div class="thumb" style="background-image: url('20160102/thumb.png')">
+          <span class="date">2016 01 02<br/>Mesh Network</span>
+        </div>
+      </a>
+      <a href="20160101/">
+        <div class="thumb" style="background-image: url('20160101/thumb.png')">
+          <span class="date">2016 01 01<br/>Breton Stripe Slit Scan</span>
+        </div>
+      </a>
+
+		</div>
+
+		<?php
+			include("../_html/footer.html");
+		?>
+
+		<script src="../_js/vendor/scrollReveal.min.js"></script>
+		<script>
+      window.sr = new scrollReveal();
+    </script>
+
+	</body>
+</html>
