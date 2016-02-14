1993ad8a5b207deddc7a43f44e7175939d305f39
Aaron Siegel <datadreamerlabs@gmail.com>
2016-02-13 17:27:17 -0800

    20160213

--- index.php
+++ index.php
@@ -26,6 +26,11 @@
 
 		<div id="content">
 
+      <a href="20160213/">
+        <div class="thumb" style="background-image: url('20160213/thumb.png')">
+          <span class="date">2016 02 13<br/>Tumbler #2</span>
+        </div>
+      </a>
 			<a href="20160212/">
 				<div class="thumb" style="background-image: url('20160212/thumb.png')">
 					<span class="date">2016 02 12<br/>Tumbler</span>
