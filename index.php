d2850932a8c5ba24034bcbd414359b2be87d8fcb
Aaron Siegel <datadreamerlabs@gmail.com>
2016-06-20 16:29:38 -0700

    20160619 - Mesh #2

--- index.php
+++ index.php
@@ -26,6 +26,11 @@
 
 		<div id="content">
 
+			<a href="20160619/">
+				<div class="thumb" style="background-image: url('20160619/thumb.png')">
+					<span class="date">2016 06 19<br/>Mesh #2</span>
+				</div>
+			</a>
 			<a href="20160618/">
 				<div class="thumb" style="background-image: url('20160618/thumb.png')">
 					<span class="date">2016 06 18<br/>Mesh</span>
