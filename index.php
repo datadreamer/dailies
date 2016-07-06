c4ce64ae9f8253bbd99989490f62f1905413d0e1
Aaron Siegel <datadreamerlabs@gmail.com>
2016-07-06 14:02:06 -0700

    20160706 - Radial Boxes

--- index.php
+++ index.php
@@ -26,6 +26,11 @@
 
 		<div id="content">
 
+			<a href="20160706/">
+				<div class="thumb" style="background-image: url('20160706/thumb.png')">
+					<span class="date">2016 07 06<br/>Radial Boxes</span>
+				</div>
+			</a>
 			<a href="20160705/">
 				<div class="thumb" style="background-image: url('20160705/thumb.png')">
 					<span class="date">2016 07 05<br/>Spiraling Boxes</span>
